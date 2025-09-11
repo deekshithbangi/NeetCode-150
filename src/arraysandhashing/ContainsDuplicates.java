90da4b82219974bfdc3a13e903bd36f3a3c40c47
DeekshithBangi <deekshithbangi1@gmail.com>
2025-09-11 13:12:39 +0530

    Easy

--- src/arraysandhashing/ContainsDuplicates.java
+++ src/arraysandhashing/ContainsDuplicates.java
@@ -5,12 +5,12 @@ import java.util.Set;
 
 public class ContainsDuplicates {
     public boolean hasDuplicate(int[] nums) {
-       /*
+        /*
         Plan - 1
         Select an element and loop through the array.
         O(n^2)
 
-       for(int i = 0; i < nums.length - 1; i++) {
+        for(int i = 0; i < nums.length - 1; i++) {
             for(int j = i + 1; j < nums.length; j++) {
                 if(nums[j] == nums[i]) {
                     return true;
@@ -20,11 +20,11 @@ public class ContainsDuplicates {
        return false;
        */
 
-    /*
+        /*
         Plan - 2
         Select and element and if the element is not in set, add it.
         if current element is in set, return true
-    */
+        */
 
         Set<Integer> uniqueElements = new HashSet<>();
 
@@ -37,7 +37,8 @@ public class ContainsDuplicates {
         }
         return false;
 
-    /*  Plan - 3
+        /*
+        Plan - 3
         just add every element into set. then
         if length of set is equal to nums, then return false, else true.
 
@@ -48,6 +49,6 @@ public class ContainsDuplicates {
         }
 
         return nums.length == uniqueElements.size() ? false : true;
-    */
+        */
     }
 }

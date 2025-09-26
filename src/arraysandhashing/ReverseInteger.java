b82f4c3ea10699b247a898425243d61bf3c7eb3a
DeekshithBangi <deekshithbangi1@gmail.com>
2025-09-26 21:22:25 +0530

    understand integer overflow concept

---
+++ src/arraysandhashing/ReverseInteger.java
@@ -0,0 +1,19 @@
+package arraysandhashing;
+
+public class ReverseInteger {
+    public int reverse(int x) {
+        int reverse = 0;
+
+        while ( x != 0) {
+            if (reverse > Integer.MAX_VALUE / 10 ||
+                reverse < Integer.MIN_VALUE / 10)
+                    return 0;
+
+                int digit = x % 10 ? x > 0 : x % -10;
+            reverse = reverse * 10 + digit;
+            x /= 10;
+        }
+        // if (org < 0) res = -(-res);
+        return reverse;
+    }
+}
